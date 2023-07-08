143638032482fedf8148cf22e1b2a93e2d055eb6
Andrea Ungerer <52411390+andirea19@users.noreply.github.com>
2023-07-08 18:37:57 +0200

    Update myscript.js
    added new function

--- myscript.js
+++ myscript.js
@@ -11,3 +11,27 @@ function validatePassword() {
         return true;
       }
     }
+
+    function validateForm() {
+      // Get form inputs
+      var name = document.forms["myForm"]["name"].value;
+      var email = document.forms["myForm"]["email"].value;
+      
+      // Check if inputs are empty
+      if (name === "") {
+        alert("Please enter your name.");
+        return false;
+      }
+      
+      if (email === "") {
+        alert("Please enter your email.");
+        return false;
+      }
+      
+      // Validate email format
+      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        alert("Please enter a valid email address.");
+        return false;
+      }
+    }
